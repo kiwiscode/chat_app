c4dced3e6a61b98e525e92a9862b236d32f702b9
kiwiscode <ayktkav@gmail.com>
2024-06-27 15:35:48 +0200

    package.json added for heroku

--- socket_server/server.js
+++ socket_server/server.js
@@ -23,28 +23,6 @@ const io = socketIo(server, {
   },
 });
 
-const { Client } = require("pg");
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-client.connect();
-
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
-
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(logger("dev"));
