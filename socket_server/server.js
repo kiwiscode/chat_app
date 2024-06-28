7998d59db0aa835baed7b0101e4466b4ed784e97
kiwiscode <ayktkav@gmail.com>
2024-06-28 14:04:38 +0200

    localhost port deleted from database_url

--- socket_server/server.js
+++ socket_server/server.js
@@ -10,7 +10,7 @@ const socketIo = require("socket.io");
 const http = require("http");
 const server = http.createServer(app);
 const FRONTEND_URL = "https://chat-app-psi-liart.vercel.app";
-
+//
 const corsOptions = {
   origin: FRONTEND_URL,
   methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
