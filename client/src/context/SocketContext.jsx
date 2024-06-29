805feaa6542d33dcdddc7847ee9896fe5b7da46e
kiwiscode <ayktkav@gmail.com>
2024-06-29 23:36:13 +0200

    socket connection url was changed

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ export const useSocket = () => {
   return useContext(SocketContext);
 };
 
-const socket = io("https://chat-app-mpi2.onrender.com");
+const socket = io("https://chatswift-vvul5.ondigitalocean.app/");
 
 export const SocketProvider = ({ children }) => {
   useEffect(() => {
