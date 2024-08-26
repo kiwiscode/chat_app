024c68d4453e233174d32ad12217b9ac8d475f50
kiwiscode <ayktkav@gmail.com>
2024-08-27 01:26:50 +0200

    improved styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import LoadingSpinner from "./Components/LoadingSpinner/LoadingSpinner";
@@ -7,6 +7,7 @@ import { ThemeProvider } from "./context/ThemeContext";
 import Main from "./pages/main/Main";
 import Dashboard from "./pages/dashboard/Dashboard";
 import { useUser } from "./context/UserContext";
+import { SearchPeopleModalProvider } from "./context/SearchPeopleModalContext";
 //
 function App() {
   const { isAuthenticatedUser } = useUser();
@@ -15,36 +16,38 @@ function App() {
     <>
       <ThemeProvider>
         <SocketProvider>
-          <div className="w-100 h-100dvh ov-x-hid ov-y-auto color-schm-l">
-            {" "}
-            <Sidebar />
-            <Suspense
-              fallback={
-                <LoadingSpinner
-                  strokeColor={"rgb(29, 155, 240)"}
-                ></LoadingSpinner>
-              }
-            >
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    isAuthenticatedUser ? (
-                      <Navigate to="/dashboard" />
-                    ) : (
-                      <Main />
-                    )
-                  }
-                />
-                <Route
-                  path="/dashboard"
-                  element={
-                    isAuthenticatedUser ? <Dashboard /> : <Navigate to="/" />
-                  }
-                />
-              </Routes>
-            </Suspense>
-          </div>
+          <SearchPeopleModalProvider>
+            <div className="w-100 h-100dvh ov-x-hid ov-y-auto color-schm-l">
+              {" "}
+              <Sidebar />
+              <Suspense
+                fallback={
+                  <LoadingSpinner
+                    strokeColor={"rgb(29, 155, 240)"}
+                  ></LoadingSpinner>
+                }
+              >
+                <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      isAuthenticatedUser ? (
+                        <Navigate to="/dashboard" />
+                      ) : (
+                        <Main />
+                      )
+                    }
+                  />
+                  <Route
+                    path="/dashboard"
+                    element={
+                      isAuthenticatedUser ? <Dashboard /> : <Navigate to="/" />
+                    }
+                  />
+                </Routes>
+              </Suspense>
+            </div>
+          </SearchPeopleModalProvider>
         </SocketProvider>
       </ThemeProvider>
     </>
